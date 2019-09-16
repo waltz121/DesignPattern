40c868f57937625d44ed1072d51bb838720de460
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2019-09-16 18:16:55 +0800

    COntinued Added Initial Tables for Database design Pattern

---
+++ DesignPatternDatabase/dbo/tables/Address.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerId] INT NOT NULL, 
+    [StreetAddress] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [State] NVARCHAR(50) NULL, 
+    [ZipCode] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Address_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id])
+)
---
+++ DesignPatternDatabase/dbo/tables/Customer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [MiddleName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL
+)
